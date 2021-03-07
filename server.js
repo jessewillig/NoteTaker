9bd32689640e0e308beabedd6c25e5cc08b08768
Jesse Willig <jessewillig@outlook.com>
2021-03-07 15:03:02 -0800

    added more const and port listener

--- server.js
+++ server.js
@@ -3,3 +3,21 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// HTML route
+const htmlRoute = require("./route/htmlRoute");
+app.use(htmlRoute);
+
+// API route
+const apiRoute = require("./route/apiRoute");
+app.use(apiRoute);
+
+// server listen
+app.listen(PORT, () => {
+    console.log(`PORT ${PORT} is listening`);
+});
