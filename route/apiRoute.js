bf893b3f53d3a17b4de939fd77c13bd2cc5abf59
Jesse Willig <jessewillig@outlook.com>
2021-03-07 15:21:42 -0800

    added get and post

--- route/apiRoute.js
+++ route/apiRoute.js
@@ -1,6 +1,31 @@
 const express = require('express');
-const router = express.Router();
+const app = express.Router();
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const { json, response } = require('express');
 
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const parseNote = JSON.parse(data);
+            res.send(parseNote);
+        }
+    });
+});
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
+        const parseNote = JSON.parse(data);
+        const mergeNote = [...parseNote, newNote];
+        const userNote = JSON.stringify(mergeNote);
+        fs.watchFile(path.join(__dirname, "../db/db.json"), userNote, (error, response) => {
+            if (error) throw error;
+            res.json({ ok: true });
+        })
+    })
+});
