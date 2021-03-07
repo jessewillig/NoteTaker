28520b20b0b18ce4a45aa0dfa92ca26d6ad8742f
Jesse Willig <jessewillig@outlook.com>
2021-03-07 15:39:15 -0800

    added html route

---
+++ route/htmlRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express.Router();
+const path = require("path");
+
+module.exports = (app) => {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+};
