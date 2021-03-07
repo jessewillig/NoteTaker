468ca7846426048d191a62daa7f724ca716e6e67
Jesse Willig <jessewillig@outlook.com>
2021-03-07 15:30:28 -0800

    added delete app

--- route/apiRoute.js
+++ route/apiRoute.js
@@ -1,31 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express.Router();
-const path = require('path');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-const { json, response } = require('express');
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const { json, response } = require("express");
 
-app.get("/api/notes", (req, res) => {
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) => {
     fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const parseNote = JSON.parse(data);
-            res.send(parseNote);
-        }
+      if (err) {
+        console.log(err);
+      } else {
+        const parseNote = JSON.parse(data);
+        res.send(parseNote);
+      }
     });
-});
+  });
 
-app.post("/api/notes", (req, res) => {
+  app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv4();
     fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
-        const parseNote = JSON.parse(data);
-        const mergeNote = [...parseNote, newNote];
-        const userNote = JSON.stringify(mergeNote);
-        fs.watchFile(path.join(__dirname, "../db/db.json"), userNote, (error, response) => {
-            if (error) throw error;
-            res.json({ ok: true });
-        })
-    })
-});
+      const parseNote = JSON.parse(data);
+      const mergeNote = [...parseNote, newNote];
+      const userNote = JSON.stringify(mergeNote);
+      fs.watchFile(
+        path.join(__dirname, "../db/db.json"),
+        userNote,
+        (err, response) => {
+          if (err) throw err;
+          res.json({ ok: true });
+        }
+      );
+    });
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+      if (err) throw err;
+      let userNote = [];
+      data = JSON.parse(data);
+      for (i = 0; i < data.length; i++) {
+        if (data[i].id !== req.params.id) {
+          userNote.push(data[i]);
+        }
+      }
+      userNote = JSON.stringify(userNote);
+      fs.watchFile(
+        path.join(__dirname, "../db/db.json"),
+        userNote,
+        (err, response) => {
+          if (err) throw err;
+          res.json({ ok: true });
+        }
+      );
+    });
+  });
+};
