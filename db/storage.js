79febd11864f6ae991eaaf14698ac3168c4955a3
Jesse Willig <jessewillig@outlook.com>
2021-03-09 19:51:18 -0800

    fixed storage and HTML router

--- db/storage.js
+++ db/storage.js
@@ -1,19 +1,19 @@
-const util = require("util");
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
+const util = require('util');
+const fs = require('fs');
+const uuid = require('uuid');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
-
 class Storage {
   read() {
-    return readFileAsync("db/db.json", "utf8");
-  };
-  write() {
-    return writeFileAsync("db/db.json", JSON.stringify(note));
-  };
+    return readFileAsync('db/db.json', 'utf8');
+  }
+  write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
   getNotes() {
     return this.read().then((notes) => {
       let parsedNotes;
+      // If notes isn't an array or can't be turned into one, send back a new empty array
       try {
         parsedNotes = [].concat(JSON.parse(notes));
       } catch (err) {
@@ -21,23 +21,22 @@ class Storage {
       }
       return parsedNotes;
     });
-  };
+  }
   addNote(note) {
     const { title, text } = note;
     if (!title || !text) {
-      throw new Error("Needs to include both a text and title");
+      throw new Error("Note 'title' and 'text' cannot be blank");
     }
-    const newNote = { title, text, id: uuidv4() };
+    const newNote = { title, text, id: uuid };
     return this.getNotes()
       .then((notes) => [...notes, newNote])
       .then((updatedNotes) => this.write(updatedNotes))
       .then(() => newNote);
-  };
+  }
   removeNote(id) {
     return this.getNotes()
       .then((notes) => notes.filter((note) => note.id !== id))
       .then((filteredNotes) => this.write(filteredNotes));
-  };
-};
-
+  }
+}
 module.exports = new Storage();
--- route/htmlRoute.js
+++ route/htmlRoute.js
@@ -1,14 +1,12 @@
-const router =  require("express").Router();
-const path = require("path");
+const path = require('path');
+const router = require('express').Router();
 
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
-  router.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-
-  router.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
-
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = router;
